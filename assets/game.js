56c0f8a403a614e98dede702358f94265c52e8d2
notsilverman <birchba@gmail.com>
2018-06-14 15:20:34 -0500

    app works with firebase (click count test)

--- assets/game.js
+++ assets/game.js
@@ -14,3 +14,22 @@
 
 
   $('.test').text('HELLO THERE')
+
+let database = firebase.database();
+let counter = 0;
+
+  $('.stillTesting').on('click', function(){
+    counter++;
+    database.ref().set({
+      clickCount: counter
+    });
+  })
+
+
+  database.ref().on("value", function(snapshot) {
+    console.log(snapshot.val());
+    $("#click-value").text(snapshot.val().clickCount);
+    counter = snapshot.val().clickCount;
+  }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
