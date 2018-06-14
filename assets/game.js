332490b54320d793601d195c2fce1091f67bfe83
notsilverman <birchba@gmail.com>
2018-06-14 15:10:08 -0500

    Created and linked pages

---
+++ assets/game.js
@@ -0,0 +1,16 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApUg7Mr6v6DwW6bjTcEIYS47J8egFqoWE",
+    authDomain: "rock-paper-scissors-35eda.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-35eda.firebaseio.com",
+    projectId: "rock-paper-scissors-35eda",
+    storageBucket: "rock-paper-scissors-35eda.appspot.com",
+    messagingSenderId: "588343723798"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+  $('.test').text('HELLO THERE')
